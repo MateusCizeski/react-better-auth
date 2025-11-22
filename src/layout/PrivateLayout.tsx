70c09ada6e9c6fb9ed18f9ec9258f23002ccf587
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-11-22 11:15:58 -0300

    Initial implementation of Preivate Layout

---
+++ src/layout/PrivateLayout.tsx
@@ -0,0 +1,38 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "@/components/ui/header";
+// import { Sidebar } from "@/components/ui/sidebar";
+// import { Footer } from "@/components/ui/footer";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+export function PrivateLayout() {
+  const { isAuthenticated } = useSelector((state: RootState) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) navigate("/login");
+  }, [isAuthenticated, navigate]);
+
+  return (
+    <div className="flex h-screen w-full">
+      {/* Sidebar */}
+      {/* <Sidebar /> */}
+
+      {/* Conteúdo + header + footer */}
+      <div className="flex flex-col flex-1">
+        {/* Header */}
+        <Header />
+
+        {/* Conteúdo dinâmico (cada página vai aqui) */}
+        <main className="flex-1 p-4 overflow-auto">
+          <Outlet />
+        </main>
+
+        {/* Footer */}
+        {/* <Footer /> */}
+      </div>
+    </div>
+  );
+}
